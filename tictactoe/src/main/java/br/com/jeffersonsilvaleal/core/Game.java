d3deb5e1a1d954dc7caee2451906e18452f987da
Jefferson <drankhar@gmail.com>
2021-05-11 20:09:44 -0300

    Tictactoe primeiro commit implementação da estrutura base, classe Game e Main criada!!

---
+++ tictactoe/src/main/java/br/com/jeffersonsilvaleal/core/Game.java
@@ -0,0 +1,9 @@
+package br.com.jeffersonsilvaleal.core;
+
+public class Game {
+
+	
+	public void play() {
+		System.out.println("Jogando...");
+	}
+}
---
+++ tictactoe/src/main/java/br/com/jeffersonsilvaleal/tictactoe/Main.java
@@ -0,0 +1,13 @@
+package br.com.jeffersonsilvaleal.tictactoe;
+
+import br.com.jeffersonsilvaleal.core.Game;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Game g = new Game();
+		g.play();
+	}
+
+}
