9e6e31bed1b21923b64a72bc5e6fe31b014b8f6d
Jefferson <drankhar@gmail.com>
2021-06-23 22:17:12 -0300

    Implementamos o método de criação de jogador!!

--- tictactoe/src/main/java/br/com/jeffersonsilvaleal/core/Game.java
+++ tictactoe/src/main/java/br/com/jeffersonsilvaleal/core/Game.java
@@ -10,10 +10,22 @@ public class Game {
 	// Cria e define o número de Jogadores de acordo com o tamanho do array de Players atualmente em 2 players
 	private Player[] players = new Player[Constants.SYMBOL_PLAYERS.length];
 	
+	
 	public void play() {
 		UI.printGameTitle();
 		
-		board.print();
-		
+		for (int i = 0; i < players.length; i++) {
+			players[i] = createPlayer(i);
+		}
+	}
+
+	// Implementado método de criação de jogador
+	private Player createPlayer(int index) {
+		String name = UI.readInput("Jogador " + (index + 1) + " =>"); 
+		char symbol =  Constants.SYMBOL_PLAYERS[index];
+		Player player = new Player(name, board, symbol);
+	
+		UI.printText("O jogador '" + name + "' vai usar o símbolo '" + symbol + "'");
+		return player;
 	}
 }
